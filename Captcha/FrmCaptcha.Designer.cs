ef41dc6770e00d9d72287e03247bee35a9ef9cba
Mohamed Saleh <MohamedSaleh1984@hotmail.com>
2020-09-24 11:01:27 -0500

    Added Solution Folder

---
+++ Captcha/FrmCaptcha.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Captcha
+{
+    partial class FrmCaptcha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(77, 68);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(171, 94);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(90, 94);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(75, 23);
+            this.btnVerify.TabIndex = 2;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(9, 94);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // FrmCaptcha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 122);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "FrmCaptcha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Captcha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Captcha/FrmCaptcha.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace Captcha
+{
+    public partial class FrmCaptcha : Form
+    {
+        private Random rand = new Random();
+        private string code;
+
+        public FrmCaptcha()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateImage();
+        }
+
+        private void CreateImage()
+        {
+            string code = GenerateRandomText();
+
+            Bitmap bitmap = new Bitmap(200, 50, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bitmap);
+           
+            Rectangle rect = new Rectangle(0, 0, 200, 50);
+
+            SolidBrush blkBrush = new SolidBrush(Color.Black);
+            SolidBrush whtBrush = new SolidBrush(Color.White);
+
+            int ipxlOffset = 0;
+
+            g.DrawRectangle(new Pen(Color.Yellow), rect);
+
+            g.FillRectangle(blkBrush, rect);
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                g.DrawString(code[i].ToString(),
+                            new Font("Tahoma", 10 + rand.Next(14, 18)),
+                            whtBrush, new PointF(10 + ipxlOffset, 10));
+
+                ipxlOffset += 20;
+            }
+
+            DrawRandomLines(g);
+
+            if (File.Exists("tempimage.bmp"))
+            {
+                try
+                {
+                    File.Delete("tempimage.bmp");
+                    bitmap.Save("tempimage.bmp");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                bitmap.Save("tempimage.bmp");
+            }
+
+            g.Dispose();
+            
+            bitmap.Dispose();
+            
+            pictureBox1.Image = Image.FromFile("tempimage.bmp");
+        }
+
+        private void DrawRandomLines(Graphics g)
+        {
+            SolidBrush green = new SolidBrush(Color.Gray);
+            for (int i = 0; i < 10 ; i++)
+            {
+                g.DrawLines(new Pen(green, 2), GetRandomPoints());
+            }
+        }
+
+        private Point[] GetRandomPoints()
+        {
+            Point[] points = { new Point(rand.Next(10, 150), rand.Next(10, 150)), 
+                               new Point(rand.Next(10, 100), rand.Next(10, 100)) };
+            return points;
+        }
+       
+        private string GenerateRandomText()
+        {
+            string randomText = "";
+
+            if (String.IsNullOrEmpty(code))
+            {
+                string alphabets = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                
+                Random r = new Random();
+
+                for (int j = 0; j <= 5 ; j++)
+                {
+                    randomText = randomText + alphabets[r.Next(alphabets.Length)];
+                   
+                }
+                code = randomText.ToString();
+            }
+            return code;
+        }
+        
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text.Equals(code))
+                MessageBox.Show("Correct","",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Not Correct", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Dispose();
+            code = "";
+            CreateImage();
+        }
+
+        
+    }
+
+    public class CaptchaGenerator
+    {
+        private Random _Rand;
+        private  string _strCode;
+        private int _iCodeLength;
+        private  Image _BitMapImage;
+        public String _DirectoryPath { get; set; }
+        private string _GeneratedFileName { get; set; }
+        public string GetGeneratedFilePath()
+        {
+            return _DirectoryPath + "\\" + _GeneratedFileName;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="CodeLength">Length of Generated Code</param>
+        /// <param name="DirectoryPath">Location where BMP files Get generated.</param>
+        public CaptchaGenerator(int CodeLength=5,
+                                string DirectoryPath="C:\\Images")
+        {
+            _Rand = new Random();
+            _iCodeLength = CodeLength;
+            _BitMapImage = null;
+            _DirectoryPath = DirectoryPath;
+            _GeneratedFileName = "";
+            _strCode = "";
+
+        }
+
+        /// <summary>
+        /// Reset all values for the class.
+        /// </summary>
+        public void Reset()
+        {
+            _Rand = new Random();
+            _iCodeLength = 5;
+            _DirectoryPath = "C:\\Images";
+        }
+
+        /// <summary>
+        /// Generate random points to place tge characters in the Bitmap.
+        /// </summary>
+        /// <returns></returns>
+        private Point[] GetRandomPoints()
+        {
+            Point[] points = { new Point(_Rand.Next(10, 150), _Rand.Next(10, 150)),
+                               new Point(_Rand.Next(10, 100), _Rand.Next(10, 100)) };
+            return points;
+        }
+
+        /// <summary>
+        /// Generate random text.
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateRandomText()
+        {
+            string randomText = "";
+
+            if (String.IsNullOrEmpty(_strCode))
+            {
+                string alphabets = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                Random r = new Random();
+
+                for (int j = 0; j <= _iCodeLength; j++)
+                {
+                    randomText = randomText + alphabets[r.Next(alphabets.Length)];
+
+                }
+                _strCode = randomText.ToString();
+            }
+            return _strCode;
+        }
+
+        /// <summary>
+        /// Check if the given string matched with generated code.
+        /// </summary>
+        /// <param name="strInputFromUser"></param>
+        /// <returns></returns>
+        public bool IsValidCode(object strInputFromUser)
+        {
+            if(String.IsNullOrEmpty(_strCode))
+            {
+                throw new Exception("");
+            }
+
+            return _strCode.Equals(Convert.ToString(strInputFromUser)) ? true : false;
+        }
+
+        /// <summary>
+        /// Generate an Image.
+        /// </summary>
+        /// <param name="strPath"></param>
+        private void CreateImage()
+        {
+            string code = GenerateRandomText();
+
+            Bitmap bitmap = new Bitmap(200, 50, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            Rectangle rect = new Rectangle(0, 0, 200, 50);
+
+            SolidBrush blkBrush = new SolidBrush(Color.Black);
+            SolidBrush whtBrush = new SolidBrush(Color.White);
+
+            int ipxlOffset = 0;
+
+            g.DrawRectangle(new Pen(Color.Yellow), rect);
+
+            g.FillRectangle(blkBrush, rect);
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                g.DrawString(code[i].ToString(),
+                            new Font("Tahoma", 10 + _Rand.Next(14, 18)),
+                            whtBrush, new PointF(10 + ipxlOffset, 10));
+
+                ipxlOffset += 20;
+            }
+
+            DrawRandomLines(g);
+
+            string strFileName = DateTime.Now.Year.ToString() + 
+                                DateTime.Now.Month.ToString() + 
+                                DateTime.Now.Day.ToString() + 
+                                DateTime.Now.Hour.ToString() + 
+                                DateTime.Now.Minute.ToString() + 
+                                DateTime.Now.Second.ToString();
+
+            strFileName += ".bmp";
+            _GeneratedFileName = strFileName;
+
+            if(!Directory.Exists(_DirectoryPath))
+            {
+                Directory.CreateDirectory(_DirectoryPath);
+            }
+
+
+            if (File.Exists(_DirectoryPath + "\\" + _GeneratedFileName))
+            {
+                try
+                {
+                    File.Delete(_DirectoryPath + "\\" + _GeneratedFileName);
+                    bitmap.Save(_DirectoryPath + "\\" + _GeneratedFileName);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error creating file." + "\n" + ex.Message);
+                }
+            }
+            else
+            {
+                bitmap.Save(_DirectoryPath + "\\" + _GeneratedFileName);
+            }
+
+            g.Dispose();
+
+            bitmap.Dispose();
+
+            _BitMapImage = Image.FromFile(_DirectoryPath + "\\" + _GeneratedFileName);
+        }
+
+        /// <summary>
+        /// Draw lines.
+        /// </summary>
+        /// <param name="g"></param>
+        private void DrawRandomLines(Graphics g)
+        {
+            SolidBrush green = new SolidBrush(Color.Gray);
+            for (int i = 0; i < 10; i++)
+            {
+                g.DrawLines(new Pen(green, 2), GetRandomPoints());
+            }
+        }
+
+        private void Check()
+        {
+            if(String.IsNullOrEmpty(this._DirectoryPath))
+            {
+                throw new Exception("You should set Directory first.");
+            }
+            if (String.IsNullOrEmpty(this._GeneratedFileName))
+            {
+                throw new Exception("No generated file");
+            }
+
+            if (String.IsNullOrEmpty(this._strCode))
+            {
+                throw new Exception("No generated code");
+            }
+
+            if(_BitMapImage == null)
+            {
+                throw new Exception("No Image generated.");
+            }
+
+        }
+
+    }
+}
