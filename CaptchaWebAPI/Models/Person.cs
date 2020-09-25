414c889e4a48718a66b77b109cd1713f0b9d50a4
Mohamed Saleh <MohamedSaleh1984@hotmail.com>
2020-09-25 11:48:39 -0500

    Added XML documentation

--- CaptchaWebAPI/Models/Person.cs
+++ CaptchaWebAPI/Models/Person.cs
@@ -5,10 +5,23 @@ using System.Web;
 
 namespace CaptchaWebAPI.Models
 {
+    /// <summary>
+    /// Represents one specific user.
+    /// </summary>
     public class Person
     {
+
+        /// <summary>
+        /// User unique id
+        /// </summary>
         public int Id { get; set; } = 0;
+        /// <summary>
+        /// User First Name
+        /// </summary>
         public string FirstName { get; set; } = "";
+        /// <summary>
+        /// User Last Name
+        /// </summary>
         public string LastName { get; set; } = "";
     }
 }
